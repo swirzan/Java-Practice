442ca7c67f3c29e208b02fadb6ec9662d5e64ba0
swirzan <swirzan@gmail.com>
2022-12-18 13:08:46 +0500

    Equal sum checker exercise

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,17 @@
+public class EqualSumChecker {
+    public static void main(String[] args) {
+        boolean test1 = hasEqualSum(1, 1, 1);
+        boolean test2 = hasEqualSum(1, 1, 2);
+        boolean test3 = hasEqualSum(1, -1, 0);
+        System.out.println(test1);
+        System.out.println(test2);
+        System.out.println(test3);
+    }
+
+    public static boolean hasEqualSum(int numOne, int numTwo, int numThree) {
+        if (numOne + numTwo == numThree) {
+            return true;
+        }
+        return false;
+    }
+}
--- src/LeapYearCalculator.java
+++ src/LeapYearCalculator.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class LeapYearCalculator {
-}
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        boolean test = isLeapYear(-1200);
+        System.out.println(test);
+
+
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 || year <= 9999) {
+            return false;
+        } else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return true;
+        }
+        return false;
+    }
+};
+
