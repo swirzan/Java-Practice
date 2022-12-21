6bc9615a559bbe68dba1124edc2ac69202be5571
swirzan <swirzan@gmail.com>
2022-12-21 19:44:19 +0500

    Leap Year Challenge

--- src/LeapYearCalculator.java
+++ src/LeapYearCalculator.java
@@ -1,13 +1,22 @@
 public class LeapYearCalculator {
     public static void main(String[] args) {
-        boolean test = isLeapYear(-1200);
-        System.out.println(test);
-
-
+        boolean test1 = isLeapYear(-1600);
+        boolean test2 = isLeapYear(1600);
+        boolean test3 = isLeapYear(2017);
+        boolean test4 = isLeapYear(2000);
+        System.out.println(test1);
+        System.out.println(test2);
+        System.out.println(test3);
+        System.out.println(test4);
     }
+    //Output
+    //false
+    //true
+    //false
+    //true
 
     public static boolean isLeapYear(int year) {
-        if (year >= 1 || year <= 9999) {
+        if (year < 1 || year > 9999) {
             return false;
         } else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             return true;
