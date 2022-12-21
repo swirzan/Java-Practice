b28cc37fa07b157e11e881d2306b1dcb69e9a5d0
swirzan <swirzan@gmail.com>
2022-12-21 19:55:40 +0500

    Teen Number Checker Challenge

--- src/TeenNumberChecker.java
+++ src/TeenNumberChecker.java
@@ -1,11 +1,24 @@
 public class TeenNumberChecker {
     public static void main(String[] args) {
-        boolean test = hasTeen(22, 23, 34);
-        boolean test2 = isTeen(13);
-        ;
+        boolean test1 = hasTeen(9, 99, 19);
+        boolean test2 = hasTeen(23, 15, 42);
+        boolean test3 = hasTeen(22, 23, 34);
+        System.out.println(test1);
         System.out.println(test2);
+        System.out.println(test3);
+
+        boolean tes4 = isTeen(9);
+        boolean test5 = isTeen(13);
+        System.out.println(tes4);
+        System.out.println(test5);
     }
 
+    //Output
+    //true
+    //true
+    //false
+    //false
+    //true
     public static boolean hasTeen(int ageOne, int ageTwo, int ageThree) {
         if (ageOne >= 13 && ageOne <= 19 || ageTwo >= 13 && ageTwo <= 19 || ageThree >= 13 && ageThree <= 19) {
             return true;
