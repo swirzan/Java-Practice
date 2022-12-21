c8527b6437fb7feb6e0878b15b01692ae01cfbc1
swirzan <swirzan@gmail.com>
2022-12-21 19:59:30 +0500

    Area Calculator Challenge

--- src/AreaCalculatorChallenge.java
+++ src/AreaCalculatorChallenge.java
@@ -1,10 +1,19 @@
 public class AreaCalculatorChallenge {
     public static void main(String[] args) {
-        double test = area(5.0);
-        System.out.println(test);
-        double test2 = area(-1.0, 4.0);
+        double test1 = area(5.0);
+        double test2 = area(-1);
+        double test3 = area(5.0, 4.0);
+        double test4 = area(-1.0, 4.0);
+        System.out.println(test1);
         System.out.println(test2);
+        System.out.println(test3);
+        System.out.println(test4);
     }
+    //Output
+    //78.53981633974483
+    //-1.0
+    //20.0
+    //-1.0
 
     public static double area(double radius) {
         if (radius < 0) {
