ab8ac0f6ce858652871640e88f61815b5b8bf897
swirzan <swirzan@gmail.com>
2022-12-21 19:31:09 +0500

    Megabytes Converter challenge

--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -1,7 +1,13 @@
 public class MegaBytesConverter {
     public static void main(String[] args) {
         printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
     }
+    //Output
+    //2500 KB = 2 MB and 452 KB
+    //Invalid Input
+    //5000 KB = 4 MB and 904 KB
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes) {
         if (kiloBytes >= 0) {
