86316a445571976303ca6f68dc97025c30a459cc
swirzan <swirzan@gmail.com>
2022-12-21 19:49:03 +0500

    Decimal Comparator Challenge

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,10 +1,19 @@
 public class DecimalComparator {
     public static void main(String[] args) {
-        boolean test = areEqualByThreeDecimalPlaces(3.175, 3.176);
-        System.out.println(test);
+        boolean test1 = areEqualByThreeDecimalPlaces(-3.1756, -3.175);
+        boolean test2 = areEqualByThreeDecimalPlaces(3.175, 3.176);
+        boolean test3 = areEqualByThreeDecimalPlaces(3.0, 3.0);
+        boolean test4 = areEqualByThreeDecimalPlaces(-3.123, 3.123);
+        System.out.println(test1);
+        System.out.println(test2);
+        System.out.println(test3);
+        System.out.println(test4);
     }
-
-    ;
+    //Output
+    //true
+    //false
+    //true
+    //false
 
     public static boolean areEqualByThreeDecimalPlaces(double numOne, double numTwo) {
         int numOneCheck = (int) (numOne * 1000);
