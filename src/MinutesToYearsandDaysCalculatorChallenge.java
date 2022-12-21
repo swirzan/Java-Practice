306bcaf8bc9342eaf2f1b1bc24a2ac0952fe3922
swirzan <swirzan@gmail.com>
2022-12-21 20:02:40 +0500

    Minutes To Years and Days Calculator Challenge

--- src/MinutesToYearsandDaysCalculatorChallenge.java
+++ src/MinutesToYearsandDaysCalculatorChallenge.java
@@ -1,8 +1,14 @@
 public class MinutesToYearsandDaysCalculatorChallenge {
     public static void main(String[] args) {
         printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
     }
 
+    //Output
+    //525600 min = 1 y and 0d
+    //1051200 min = 2 y and 0d
+    //561600 min = 1 y and 25d
     public static void printYearsAndDays(long minutes) {
         if (minutes < 0) {
             System.out.println("Invalid Value");
