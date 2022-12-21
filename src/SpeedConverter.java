f94ceaaf2225db1ef4742158ee4ee5290639409a
swirzan <swirzan@gmail.com>
2022-12-21 19:28:37 +0500

    Speed Converter Challenge

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,24 +1,36 @@
 public class SpeedConverter {
     public static void main(String[] args) {
-//        // Evaluate
-//        printConversion(1.5);
-//        printConversion(10.25);
-//        printConversion(-5.6);
-//        printConversion(25.42);
-//        printConversion(75.114);
-//    printConversion(0.0);
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+        //Output
+        //1.5 km/h = 1 mi/h
+        //10.25 km/h = 6 mi/h
+        //Invalid Value
+        //25.42 km/h = 16 mi/h
+        //75.114 km/h = 47 mi/h
 
 
-//       int test1 = (int) toMilesPerHour (1.5);
-//       double test2 = (double) toMilesPerHour (10.25);
-//       long test3 = toMilesPerHour (-5.6);
-//       long test4 = toMilesPerHour (25.42);
-//       long test5 = toMilesPerHour (75.114);
-//        System.out.println(test1 + "\n" + test2 + "\n" + test3 + "\n" + test4 + "\n" + test5);
-
-
-
+        int test1 = (int) toMilesPerHour(1.5);
+        double test2 = (double) toMilesPerHour(10.25);
+        long test3 = toMilesPerHour(-5.6);
+        long test4 = toMilesPerHour(25.42);
+        long test5 = toMilesPerHour(75.114);
+        System.out.println(test1);
+        System.out.println(test2);
+        System.out.println(test3);
+        System.out.println(test4);
+        System.out.println(test5);
+        //Output
+        //1
+        //6.0
+        //-1
+        //16
+        //47
     }
+
     public static long toMilesPerHour(double kilometersPerHour) {
         double MILES_TO_KILOMETER_UNIT = 1.609;
         if (kilometersPerHour >= 0) {
@@ -27,6 +39,7 @@ public class SpeedConverter {
         }
         return -1;
     }
+
     public static void printConversion(double kilometersPerHour) {
         long milesPerHour = toMilesPerHour(kilometersPerHour);
         if (milesPerHour >= 0) {
